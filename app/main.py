0cee5acb152689e85b515afb37dbaf3a3fa67328
Calychas <kacper.lesniara@gmail.com>
2021-01-12 01:35:28 +0100

    fix list typing in main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from models import *
+from typing import List
 
 from os import getenv
 from data import load_users, get_points
@@ -13,10 +14,10 @@ users = load_users()
 
 
 @app.get("/user")
-async def get_users() -> [TwitterUser]:
+async def get_users() -> List[TwitterUser]:
     return users
 
 
 @app.get("/point3d")
-async def get_points_3d() -> [TwitterPoint3d]:
+async def get_points_3d() -> List[TwitterPoint3d]:
     return get_points(users)

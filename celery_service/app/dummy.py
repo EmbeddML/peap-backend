a4cf9482a88fd7f76ab758ebc2a99969737b593d
Piotr Gramacki <p.gramacki@gmail.com>
2021-01-26 00:13:58 +0100

    more files cleaning

--- celery_service/app/dummy.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery, signature
-import celery_conf
-
-app = Celery()
-app.config_from_object(celery_conf)
-
-result = signature('get_tweets', args=('bbudka', )).delay()
-
-print(result.get())
